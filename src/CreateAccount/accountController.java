3abe13f75c7ad7c9388269d57bc865e2502e5bb7
Byron Mouen <42878404+Birohn@users.noreply.github.com>
2018-09-20 15:37:39 -0400

    Added create account
    Users can now create an account

--- src/CreateAccount/accountController.java
+++ src/CreateAccount/accountController.java
@@ -1,19 +1,34 @@
 package CreateAccount;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+import dbConn.DatabaseConnection;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class accountController {
-
+    @FXML
+    private TextField userName;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private PasswordField password;
     @FXML
     private Hyperlink goBackToLogin;
-
+    private DatabaseConnection connected;
     @FXML
     public void handleToLogin() { // Goes back to Login (Needs to be put into different controller)
         Stage stage = (Stage) this.goBackToLogin.getScene().getWindow();
@@ -31,5 +46,31 @@ public class accountController {
         }
 
     }
+    public void initialize(URL url, ResourceBundle rb) {
+        this.connected = new DatabaseConnection();
+    }
+    @FXML
+    private void newAccount(ActionEvent event) {
+        String insertLoginInfo = "INSERT INTO login(username,password) VALUES (?, ?)";
+        String insertUserInfo = "INSERT INTO userInfo(fname,lname) VALUES (?, ?)";
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(insertLoginInfo);
+            stmt.setString(1,this.userName.getText());
+            stmt.setString(2,this.password.getText());
+            stmt.execute();
+            PreparedStatement stmt2 = conn.prepareStatement(insertUserInfo);
+            stmt2.setString(1,this.firstName.getText());
+            stmt2.setString(2,this.lastName.getText());
+            stmt2.execute();
+            conn.close();
+        }
+        catch(SQLException ex) {
+            ex.printStackTrace();
+        }
 
+
+
+
+    }
 }
