8857f8f5dfd74ceccd7d647b93f65200f3a5003c
Byron Mouen <42878404+Birohn@users.noreply.github.com>
2018-09-28 13:47:59 -0400

    Refinement of Create Account function
    Function now checks to see if fields are filled in and if password is correct.

--- src/CreateAccount/accountController.java
+++ src/CreateAccount/accountController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import dbConn.DatabaseConnection;
@@ -26,6 +27,10 @@ public class accountController {
     private TextField lastName;
     @FXML
     private PasswordField password;
+    @FXML
+    private PasswordField passwordCheck;
+    @FXML
+    private Label errorMessage;
     @FXML
     private Hyperlink goBackToLogin;
     private DatabaseConnection connected;
@@ -53,23 +58,33 @@ public class accountController {
     private void newAccount(ActionEvent event) {
         String insertLoginInfo = "INSERT INTO login(username,password) VALUES (?, ?)";
         String insertUserInfo = "INSERT INTO userInfo(fname,lname) VALUES (?, ?)";
-        try {
-            Connection conn = DatabaseConnection.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(insertLoginInfo);
-            stmt.setString(1,this.userName.getText());
-            stmt.setString(2,this.password.getText());
-            stmt.execute();
-            PreparedStatement stmt2 = conn.prepareStatement(insertUserInfo);
-            stmt2.setString(1,this.firstName.getText());
-            stmt2.setString(2,this.lastName.getText());
-            stmt2.execute();
-            conn.close();
+        if(this.userName.getText().isEmpty()||this.password.getText().isEmpty()||this.firstName.getText().isEmpty()||this.lastName.getText().isEmpty()) {
+        errorMessage.setText("There is an empty field!");
         }
-        catch(SQLException ex) {
-            ex.printStackTrace();
-        }
+        else if(!this.passwordCheck.getText().equals(this.password.getText())) {
+        errorMessage.setText("Your password does not match! Check again!");
 
 
+        }
+        else {
+            try {
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(insertLoginInfo);
+                stmt.setString(1, this.userName.getText());
+                stmt.setString(2, this.password.getText());
+                stmt.execute();
+                PreparedStatement stmt2 = conn.prepareStatement(insertUserInfo);
+                stmt2.setString(1, this.firstName.getText());
+                stmt2.setString(2, this.lastName.getText());
+                stmt2.execute();
+                conn.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            handleToLogin();
+
+        }
 
 
     }
