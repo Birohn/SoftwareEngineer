b6ab7f1c8e2afc2800e46cba2ea2cf4261a40f28
Harnoor Singh <hsingh9@student.gsu.edu>
2018-11-22 13:15:39 -0500

    Login Test Added

---
+++ client/src/main/java/com/client/login/LoginControllerTest.java
@@ -0,0 +1,47 @@
+package com.client.login;
+
+import com.byron.MainLauncher.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class LoginControllerTest extends ApplicationTest {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent mainNode = FXMLLoader.load(Main.class.getClassLoader().getResource("views/Login.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    @Test
+    public void loginAndLogout() {
+        // Login using userId and password
+        clickOn("#usernameField");
+        write("Harnoor");
+        clickOn("#passwordField");
+        write("password");
+        clickOn("#loginButton");
+        // Logout
+//        clickOn("#logout");
+    }
+}
